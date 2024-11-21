881fb17fab91bc232082e4c7fb2e31dcf05dd89d
Ste <mrbarrow@hotmail.co.uk>
2024-11-21 20:38:15 +0000

    high school sweetheart

---
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,36 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    String.trim(name) |> String.at(0)
+  end
+
+  def initial(name) do
+    first_letter(name) |> String.upcase() |> Kernel.<>(".")
+  end
+
+  def initials(full_name) do
+    names = String.split(full_name)
+    initial(Enum.at(names, 0)) <> " " <> initial(Enum.at(names, 1))
+  end
+
+  def pair(full_name1, full_name2) do
+    first_person_initials = initials(full_name1)
+    second_person_initials = initials(full_name2)
+
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{first_person_initials}  +  #{second_person_initials}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
